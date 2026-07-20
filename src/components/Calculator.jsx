81ca63accd69ef765f73bfd0a5f20ea354fb7b58
Yadin Nou <nouyadin@gmail.com>
2026-07-20 10:15:04 +1000

    completed dot function

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -17,30 +17,42 @@ export const Calculator = ({ displayData }) => {
     }
     // condition with "."
     if (btn === ".") {
-      if (data.slice(-1) === btn) {
+      // if (data.slice(-1) === btn || ) {
+      //   btn = "";
+      // } else if (data.includes(btn)) {
+      //   operator.forEach((item, index) => {
+      //     if (String(data).lastIndexOf(item, -1)) {
+      //       if (data.includes(item)) {
+      //         console.log(data.split(item));
+      //         return;
+      //       }
+      //     }
+      //   });
+      let lastString = "";
+      let lastIndex = [];
+      operator.forEach((item, index) => {
+        //check data has operator or not, if have get only the last index
+        // then slice the data by the last index
+        if (data.includes(item)) {
+          lastIndex.push(Number(data.lastIndexOf(item)));
+        }
+      });
+      //...max can't accept the array, so it accept individual numbers as separate arguments, not an array:
+      //find the max number in lastIndex as data have mulitple operator
+      let maxIndex = Math.max(...lastIndex);
+      lastString = data.slice(maxIndex + 1);
+      //console.log(lastString);
+      if (lastString.includes(btn)) {
+        // console.log(lastIndex, "Last string has dot", lastString);
         btn = "";
-      } else if (data.includes(btn)) {
-        operator.forEach((item, index) => {
-          if (String(data).lastIndexOf(item, -1)) {
-            if (data.includes(item)) {
-              console.log(data.split(item));
-              return;
-            }
-          }
-        });
-        //  console.log(opHas);
-        // if (!data.includes(operator)) {
-        //   console.log("no operator");
-        //   btn = "";
-        // } else {
-        //   console.log("data include: ", btn);
-        // }
+      } else {
+        // console.log("lastString no Dot");
+        btn = btn;
       }
     }
     // condition with operator =
     if (btn === "=") {
     }
-
     // condition with any operator
     if (operator.includes(btn)) {
     }
